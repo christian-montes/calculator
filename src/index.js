33270fad56645d75a600dbac0d1186a6fc3fef43
Christian Montes <christian.montes8991@gmail.com>
2021-03-08 01:34:54 -0800

    expression logic built

--- src/index.js
+++ src/index.js
@@ -25,21 +25,21 @@ function Calculator () {
 
 
   const inputFunction = (e) =>  {
-    if (display === '0' && e.target.value === '.') {
+    if (display === '0' && e.currentTarget.value === '.') {
       setDisplay(display + '.');
-    } else if (display == '0' && e.target.value != '.') {
-      setDisplay(e.target.value)
+    } else if (display == '0' && e.currentTarget.value != '.') {
+      setDisplay(e.currentTarget.value)
     } else {
-      setDisplay(display + e.target.value)
+      setDisplay(display + e.currentTarget.value)
     }
   }
 
 
   const handleInput = (e) => {
-    // setButtonID(e.target.id);
-    const x = e.target.value;
+    // setButtonID(e.currentTarget.id);
+    const x = e.currentTarget.value;
     const prevPushed = document.getElementById(buttonID)
-    console.log(x, buttonID, e.target.type, prevPushed)
+    console.log(x, buttonID, e.currentTarget.type, prevPushed)
  
 
     if (prevPushed === null) {
@@ -49,7 +49,7 @@ function Calculator () {
     }
 
 
-    setButtonID(e.target.id);
+    setButtonID(e.currentTarget.id);
   }
 
   // const handleFirstEntry = (value, name, operator) => {
@@ -63,6 +63,13 @@ function Calculator () {
   {/*IMPORTANT
   run the cleanup each time the operand is not part of the pairs
   only execute the cleanup function if the previous operand is different*/}
+  // const run = {operation: 'add', execute: false};
+  const handleEquals = () => {
+    console.log(sum, prod, total, buttonID);
+    // if (run.execute && run.operation === 'add' || run.operation === 'subtract') {
+    //   setDisplay()
+    // }
+  }
 
 
   // creating event handlers for the four functions
@@ -75,47 +82,62 @@ function Calculator () {
 
     // parse the string in the display
     const numEntered = parseFloat(display) * 100 / 100
-    // !operand && handleFirstEntry(numEntered, e.target.id, 'add');
+    // !operand && handleFirstEntry(numEntered, e.currentTarget.id, 'add');
     if (!operand) {
       setSum(numEntered);
-      setButtonID(e.target.id);
+      setButtonID(e.currentTarget.id);
       setOperand('add');
     }
 
 
     // if (operand && operand === 'add') {
-    //   setSum(sum + e.target.value);
+    //   setSum(sum + e.currentTarget.value);
     //   setOperand('add');
     // } else if (operand && operand === 'subtract') {
-    //   setSum(sum - e.target.value);
+    //   setSum(sum - e.currentTarget.value);
     //   setOperand('add')
     // }
     
     if (operand) {
       const elem = document.getElementById(buttonID)
-      if (elem.type === e.target.type && buttonID === e.target.id) {
+      if (elem.type === e.currentTarget.type && buttonID === e.currentTarget.id) {
         // reverse the operation and set the operand to the new one
         console.log('Clicking the + operator!');
 
-      } else if (elem.type === e.target.type && buttonID != e.target.id) {
+      } else if (elem.type === e.currentTarget.type && buttonID != e.currentTarget.id) {
         // call opposite of addition
         // operations[elem.id]['opposite'](numEntered);
         // setButtonID after reversing other operation
-        setButtonID(e.target.id);
+        setButtonID(e.currentTarget.id);
         // setSum(sum + numEntered);
 
       } else {
-        operand === 'add' ? setSum(sum + numEntered)
-       :operand === 'subtract' ? setSum(sum - numEntered)
-       :operand === 'multiply' ? setProd(prod * numEntered)
-       :setProd(prod / numEntered)
+        let a = sum + numEntered,
+        s = sum - numEntered, 
+        m = !total ? sum && sum + prod * numEntered : total + prod * numEntered, 
+        d = !total ? prod / numEntered : total + prod * numEntered;
+        // if (operand === 'add') {
+        //   setSum(sum + numEntered);
+        //   setDisplay(x.toString())
+        // } else if (operand === 'subtract') {
+        //   setSum(sum - numEntered);
+        //   setDisplay(x.toString())
+        // } else if (operand === 'multiply') {
+        //   setProd(prod * numEntered)
+        // } else {setProd(prod / numEntered)}
+        operand === 'add' ? (setSum(sum + numEntered), setDisplay(a.toString()))
+       :operand === 'subtract' ? (setSum(sum - numEntered), setDisplay(s.toString()))
+       :operand === 'multiply' ? (
+         setProd(prod * numEntered), 
+         setDisplay(m.toString()))
+       :(setProd(prod / numEntered), setDisplay(d.toString()))
         // handle the operators arithmetic
       }
       setOperand('add')
-      setButtonID(e.target.id)
-      console.log(operand, buttonID, e.target.id, e.target.type)
+      setButtonID(e.currentTarget.id)
+      // console.log(operand, buttonID, e.currentTarget.id, e.currentTarget.type, sum)
     }
-    console.log(operand, buttonID, e.target.id, e.target.type)
+    console.log(total, sum, prod)
   }
 
 
@@ -128,25 +150,25 @@ function Calculator () {
 
 
   const numEntered = parseFloat(display) * 100 / 100
-  // !operand && handleFirstEntry(numEntered, e.target.id, 'add');
+  // !operand && handleFirstEntry(numEntered, e.currentTarget.id, 'add');
     if (!operand) {
       setSum(numEntered);
-      setButtonID(e.target.id);
+      setButtonID(e.currentTarget.id);
       setOperand('subtract');
     }
 
 
     if (operand) {
       const elem = document.getElementById(buttonID)
-      if (elem.type === e.target.type && buttonID === e.target.id) {
+      if (elem.type === e.currentTarget.type && buttonID === e.currentTarget.id) {
         // reverse the operation and set the operand to the new one
         console.log('Clicking the - operator!');
 
-      } else if (elem.type === e.target.type && buttonID != e.target.id) {
+      } else if (elem.type === e.currentTarget.type && buttonID != e.currentTarget.id) {
         // call opposite of addition
         // operations[elem.id]['opposite'](numEntered);
         // setButtonID after reversing other operation
-        setButtonID(e.target.id);
+        setButtonID(e.currentTarget.id);
         console.log(buttonID);
         // setSum(sum - numEntered);
 
@@ -158,7 +180,7 @@ function Calculator () {
         // handle the operators arithmetic
       }
       setOperand('subtract')
-      setButtonID(e.target.id)
+      setButtonID(e.currentTarget.id)
       console.log(operand, buttonID)
     }
   }
@@ -175,36 +197,39 @@ function Calculator () {
     // setOperand('multiply');
 
     const numEntered = parseFloat(display) * 100 / 100
-    // !operand && handleFirstEntry(numEntered, e.target.id, 'add');
+    // !operand && handleFirstEntry(numEntered, e.currentTarget.id, 'add');
     if (!operand) {
       setProd(numEntered);
-      setButtonID(e.target.id);
+      setButtonID(e.currentTarget.id);
       setOperand('multiply');
     }
 
     if (operand) {
       const elem = document.getElementById(buttonID)
-      if (elem.type === e.target.type && buttonID === e.target.id) {
+      if (elem.type === e.currentTarget.type && buttonID === e.currentTarget.id) {
         // reverse the operation and set the operand to the new one
         console.log('Clicking the x operator!');
 
-      } else if (elem.type === e.target.type && buttonID != e.target.id) {
+      } else if (elem.type === e.currentTarget.type && buttonID != e.currentTarget.id) {
         // call opposite of addition
         // operations[elem.id]['opposite'](numEntered);
         // setButtonID after reversing other operation
-        setButtonID(e.target.id);
+        setButtonID(e.currentTarget.id);
         console.log(buttonID);
         // setSum(sum - numEntered);
 
       } else {
-        operand === 'add' ? () => {setTotal(total + prod + sum); setProd(numEntered)}
-       :operand === 'subtract' ? () => {setTotal(total + prod + sum); setProd(-numEntered)}
+        operand === 'add' ? (
+          prod !== 1 && setTotal(total + prod + sum), 
+          // prod === 1 && setTotal(total + prod + sum), 
+          setProd(numEntered))
+       :operand === 'subtract' ? (setTotal(total + prod + sum), setProd(-numEntered))
        :operand === 'multiply' ? setProd(prod * numEntered)
        :setProd(prod / numEntered)
         // handle the operators arithmetic
       }
       setOperand('multiply')
-      setButtonID(e.target.id)
+      setButtonID(e.currentTarget.id)
       console.log(operand, buttonID)
     }
 
@@ -217,24 +242,24 @@ function Calculator () {
     // setOperand('divide')
 
     const numEntered = parseFloat(display) * 100 / 100
-    // !operand && handleFirstEntry(numEntered, e.target.id, 'add');
+    // !operand && handleFirstEntry(numEntered, e.currentTarget.id, 'add');
     if (!operand) {
       setProd(numEntered);
-      setButtonID(e.target.id);
+      setButtonID(e.currentTarget.id);
       setOperand('divide');
     }
 
     if (operand) {
       const elem = document.getElementById(buttonID)
-      if (elem.type === e.target.type && buttonID === e.target.id) {
+      if (elem.type === e.currentTarget.type && buttonID === e.currentTarget.id) {
         // reverse the operation and set the operand to the new one
         console.log('Clicking the / operator!');
 
-      } else if (elem.type === e.target.type && buttonID != e.target.id) {
+      } else if (elem.type === e.currentTarget.type && buttonID != e.currentTarget.id) {
         // call opposite of addition
         // operations[elem.id]['opposite'](numEntered);
         // setButtonID after reversing other operation
-        setButtonID(e.target.id);
+        setButtonID(e.currentTarget.id);
         console.log(buttonID);
         // setSum(sum - numEntered);
 
@@ -247,14 +272,14 @@ function Calculator () {
         // handle the operators arithmetic
       }
       setOperand('divide');
-      setButtonID(e.target.id);
+      setButtonID(e.currentTarget.id);
       console.log(operand, buttonID);
     }
 
   }
 
   // const compareOperands = (e) => {
-  //   switch (e.target.id) {
+  //   switch (e.currentTarget.id) {
   //     case 'divide':
   //       'divide' === operand || operand === 'multiply' && handleDivision();
   //       break;
@@ -278,118 +303,124 @@ function Calculator () {
 
   return (
     <div>
-      <div className='container'>
-        <span className='d-flex flex-row col-12 mt-4'>Calculator App</span>
-        <div className='total d-flex justify-content-end col-12'>
-          {display}
-        </div>
-        <div className='row-1 d-flex flex-row col-12'>
-          <button className='col btn btn-danger rounded-0 border-dark'>AC</button>
-          <button className='col btn btn-primary rounded-0 border-dark'>+/-</button>
-          <button className='col btn btn-primary rounded-0 border-dark'>%</button>
-          <button 
-            id='divide' 
-            className='col btn btn-primary rounded-0 border-dark'
-            onClick={(e) => handleDivision(e)}
-            type='mathOp'>&divide;
+      <div className='container col-sm-6'>
+        <div className='card pb-2'>
+          <span className='d-flex flex-row col-12 my-3'>Calculator</span>
+          <div className='row-0 d-flex flex-row col-12'>
+            <span className='col display d-flex justify-content-end'>{display}</span>
+          </div>
+          <div className='row-1 d-flex flex-row col-12'>
+            <button className='col btn btn-danger rounded-0 border-dark'><span>AC</span></button>
+            <button className='col btn btn-primary rounded-0 border-dark'><span>+/-</span></button>
+            <button className='col btn btn-primary rounded-0 border-dark'><span>%</span></button>
+            <button 
+              id='divide' 
+              className='col btn btn-primary rounded-0 border-dark'
+              onClick={(e) => handleDivision(e)}
+              type='mathOp'><span>&divide;</span>
+              </button>
+          </div>
+          <div className='row-2 d-flex flex-row col-12'>
+            <button 
+              className='col btn btn-secondary rounded-0 border-dark'
+              value={7}
+              id='seven'
+              type='button'
+              onClick={(e) => {handleInput(e)}}><span>7</span>
             </button>
-        </div>
-        <div className='row-2 d-flex flex-row col-12'>
-          <button 
-            className='col btn btn-secondary rounded-0 border-dark'
-            value={7}
-            id='seven'
-            type='button'
-            onClick={(e) => {handleInput(e)}}>7
-          </button>
-          <button className='col btn btn-secondary rounded-0 border-dark'
-            value={8}
-            id='eight'
-            type='button'
-            onClick={(e) => handleInput(e)}>8
-          </button>
-          <button className='col btn btn-secondary rounded-0 border-dark'
-            value={9}
-            id='nine'
-            type='button'
-            onClick={(e) => handleInput(e)}>9
-          </button>
-          <button 
-            id='multiply'
-            className='col btn btn-primary rounded-0 border-dark'
-            onClick={(e) => handleMultiplication(e)}
-            type='mathOp'>x
-          </button>
-        </div>
-        <div className='row-3 d-flex flex-row col-12'>
-          <button className='col btn btn-secondary rounded-0 border-dark'
-            value={4}
-            id='four'
-            type='button'
-            onClick={(e) => handleInput(e)}>4
-          </button>
-          <button className='col btn btn-secondary rounded-0 border-dark'
-            value={5}
-            id='five'
-            type='button'
-            onClick={(e) => handleInput(e)}>5
-          </button>
-          <button className='col btn btn-secondary rounded-0 border-dark'
-            value={6}
-            id='six'
-            type='button'
-            onClick={(e) => handleInput(e)}>6
-          </button>
-          <button 
-            id='subtract' 
-            className='col btn btn-primary rounded-0 border-dark'
-            onClick={(e) => handleSubtraction(e)}
-            type='mathOp'>&minus;
-          </button>
-        </div>
-        <div className='row-4 d-flex flex-row col-12'>
-          <button className='col btn btn-secondary rounded-0 border-dark'
-            value={1}
-            id='one'
-            type='button'
-            onClick={(e) => handleInput(e)}>1
-          </button>
-          <button className='col btn btn-secondary rounded-0 border-dark'
-            value={2}
-            id='two'
-            type='button'
-            onClick={(e) => handleInput(e)}>2
-          </button>
-          <button className='col btn btn-secondary rounded-0 border-dark'
-            value={3}
-            id='three'
-            type='button'
-            onClick={(e) => handleInput(e)}>3
-          </button>
-          <button 
-            id='add' 
-            className='col btn btn-primary rounded-0 border-dark'
-            onClick={(e) => handleAddition(e)}
-            type='mathOp'>+
-          </button>
-        </div>
-        <div className='row-5 d-flex flex-row col-12'>
-          <button className='col btn btn-primary rounded-0 border-dark'
-            value={0}
-            id='zero'
-            type='button'
-            onClick={(e) => display != '0' ? handleInput(e) : console.log('no multiple zeroes')}>0
-          </button>
-          <button className='col btn btn-primary rounded-0 border-dark'
-            value={'.'}
-            id='period'
-            type='number'
-            onClick={(e) => display.indexOf('.') < 0 
-                      ? handleInput(e) 
-                      : console.log('decimal present')}>.
-          </button>
-          <button className='col-3 btn btn-success rounded-0 border-dark'>=
-          </button>
+            <button className='col btn btn-secondary rounded-0 border-dark'
+              value={8}
+              id='eight'
+              type='button'
+              onClick={(e) => handleInput(e)}><span>8</span>
+            </button>
+            <button className='col btn btn-secondary rounded-0 border-dark'
+              value={9}
+              id='nine'
+              type='button'
+              onClick={(e) => handleInput(e)}><span>9</span>
+            </button>
+            <button 
+              id='multiply'
+              className='col btn btn-primary rounded-0 border-dark'
+              onClick={(e) => handleMultiplication(e)}
+              type='mathOp'><span>x</span>
+            </button>
+          </div>
+          <div className='row-3 d-flex flex-row col-12'>
+            <button className='col btn btn-secondary rounded-0 border-dark'
+              value={4}
+              id='four'
+              type='button'
+              onClick={(e) => handleInput(e)}><span>4</span>
+            </button>
+            <button className='col btn btn-secondary rounded-0 border-dark'
+              value={5}
+              id='five'
+              type='button'
+              onClick={(e) => handleInput(e)}><span>5</span>
+            </button>
+            <button className='col btn btn-secondary rounded-0 border-dark'
+              value={6}
+              id='six'
+              type='button'
+              onClick={(e) => handleInput(e)}><span>6</span>
+            </button>
+            <button 
+              id='subtract' 
+              className='col btn btn-primary rounded-0 border-dark'
+              onClick={(e) => handleSubtraction(e)}
+              type='mathOp'><span>&minus;</span>
+            </button>
+          </div>
+          <div className='row-4 d-flex flex-row col-12'>
+            <button className='col btn btn-secondary rounded-0 border-dark'
+              value={1}
+              id='one'
+              type='button'
+              onClick={(e) => handleInput(e)}><span>1</span>
+            </button>
+            <button className='col btn btn-secondary rounded-0 border-dark'
+              value={2}
+              id='two'
+              type='button'
+              onClick={(e) => handleInput(e)}><span>2</span>
+            </button>
+            <button className='col btn btn-secondary rounded-0 border-dark'
+              value={3}
+              id='three'
+              type='button'
+              onClick={(e) => handleInput(e)}><span>3</span>
+            </button>
+            <button 
+              id='add' 
+              className='col btn btn-primary rounded-0 border-dark'
+              onClick={(e) => handleAddition(e)}
+              type='mathOp'><span>+</span>
+            </button>
+          </div>
+          <div className='row-5 d-flex flex-row col-12 mb-2'>
+            <button className='col btn btn-primary rounded-0 border-dark'
+              value={0}
+              id='zero'
+              type='button'
+              onClick={(e) => display != '0' ? handleInput(e) : console.log('no multiple zeroes')}>
+                <span>0</span>
+            </button>
+            <button className='col btn btn-primary rounded-0 border-dark'
+              value={'.'}
+              id='decimal'
+              type='button'
+              onClick={(e) => display.indexOf('.') < 0 
+                        ? handleInput(e) 
+                        : console.log('decimal present')}><span>.</span>
+            </button>
+            <button 
+            id='equals'
+            className='col-3 btn btn-success rounded-0 border-dark'
+            onClick={(e) => {handleEquals(e)}}><span>=</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
